9def6d40e5bb084018dd73401ee7e86597287ab0
ochianaa <ochianasepthi23@gmail.com>
2025-03-20 23:27:30 +0800

    melengkapi endpoint dari API menggunakan node.js sesuai dengan spesifikasi openAPI (get, put, delete, post)

--- index.js
+++ index.js
@@ -18,7 +18,60 @@ app.get('/users', (req, res) => {
             res.status(500).send('Internal Server Error');
             return;
         }
-        res.json(result);
+        res.json(results);
+    });
+});
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    db.query('SELECT * FROM users WHERE id = ?', [id], (err, results) => {
+        if (err) {
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        res.json(results);
+    });
+});
+
+app.put('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const name = req.query.name;
+    const email = req.query.email;
+    const age = req.query.age;
+    db.query('UPDATE users SET name = ?, email = ?, age = ? WHERE id = ?', [name, email, age, id], (err, results) => {
+        if (err) {
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        res.json(results);
+    });
+});
+
+app.delete('/users/:id', (req, res) => {
+    const id = req.params.id;
+    db.query('DELETE FROM users WHERE id = ?', [id], (err, results) => {
+        if (err) {
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        res.json(results);
+    });
+});
+
+app.post('/user', (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const age = req.body.age;
+    db.query('INSERT INTO users (name, email, age) VALUES (?, ?, ?)', [name, email, age], (err, results) => {
+        if (err) {
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        res.json(results);
     });
 });
 
