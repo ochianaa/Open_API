411c8694bc7d4473ca4d016eb3a9b60c0ff983ce
ochianaa <ochianasepthi23@gmail.com>
2025-03-20 16:22:57 +0800

    mengganti port 3000 menjadi 4000

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-import http from 'http';
+import express from 'express';
 import mysql from 'mysql2';
 
 const db = mysql.createConnection({ host: "localhost", user: "root", database: "openapi", password: "" });
+const app = express();
 
-const server = http.createServer((req, res) => {
-    db.query('SELECET * FROM user', (err, result) => {
+app.get('/users', (req, res) => {
+    db.query('SELECT * FROM users', (err, result) => {
         if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error');
-        return;
+            res.status(500).send('Internal Server Error');
+            return;
         }
-
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(results));
+        res.json(result);
     });
 });
-server.listen(3000, () => console.log('Server berjalan di http://localhost:3000'));
+app.listen(4000, () => console.log('Server berjalan di http://localhost:4000'));
